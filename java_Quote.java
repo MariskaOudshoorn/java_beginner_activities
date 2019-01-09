df95b3ba3b8c114129ca796c9113915fc1666265
Mariska Oudshoorn <moudshoorn@sogyo.nl>
2019-01-09 08:43:19 +0100

    Finished java_Quote assignment

--- java_Quote.java
+++ java_Quote.java
@@ -26,53 +26,89 @@ public class java_Quote{
 		LocalDate localDate = LocalDate.now();
 		int day = localDate.getDayOfMonth();
 		int month = localDate.getMonth().getValue();
-		//int day = 4;
-		//int month = 12;
 		int daysbefore = 0;
 		int daysYear = 0;
-		System.out.println("month is " + month);
-		System.out.println("day is " + day);
-		switch(month){
-			case 1: daysbefore = 0;
-				break;
-			case 2: daysbefore = 31;
-				break;
-			case 3: daysbefore = 59;
-				break;
-			case 4: daysbefore = 90;
-				break;
-			case 5: daysbefore = 120;
-				break;
-			case 6: daysbefore = 151;
-				break;
-			case 7: daysbefore = 181;
-				break;
-			case 8: daysbefore = 212;
-				break;
-			case 9: daysbefore = 242;
-				break;
-			case 10: daysbefore = 273;
-				break;
-			case 11: daysbefore = 303;
-				break;
-			case 12: daysbefore = 334;
-				break;
+		System.out.print("Today it is: " + day);
+		System.out.println("/" + month);
+		if(localDate.isLeapYear()){
+			daysbefore = leapYearDays(month);
+		}
+		else {
+			daysbefore = daysBefore(month);		
 		}
 		daysYear = daysbefore + day;
 		return daysYear;
     }
 
+    public int daysBefore(int month){
+    	int daysbefore = 0;
+    	switch(month){
+				case 1: daysbefore = 0;
+					break;
+				case 2: daysbefore = 31;
+					break;
+				case 3: daysbefore = 59;
+					break;
+				case 4: daysbefore = 90;
+					break;
+				case 5: daysbefore = 120;
+					break;
+				case 6: daysbefore = 151;
+					break;
+				case 7: daysbefore = 181;
+					break;
+				case 8: daysbefore = 212;
+					break;
+				case 9: daysbefore = 242;
+					break;
+				case 10: daysbefore = 273;
+					break;
+				case 11: daysbefore = 303;
+					break;
+				case 12: daysbefore = 334;
+					break;
+		}
+		return daysbefore;
+    }
+
+    public int leapYearDays(int month){
+    	int daysbefore = 0;
+		switch(month){
+			case 1: daysbefore = 0;
+				break;
+			case 2: daysbefore = 31;
+				break;
+			case 3: daysbefore = 60;
+				break;
+			case 4: daysbefore = 91;
+				break;
+			case 5: daysbefore = 121;
+				break;
+			case 6: daysbefore = 152;
+				break;
+			case 7: daysbefore = 182;
+				break;
+			case 8: daysbefore = 213;
+				break;
+			case 9: daysbefore = 243;
+				break;
+			case 10: daysbefore = 274;
+				break;
+			case 11: daysbefore = 304;
+				break;
+			case 12: daysbefore = 335;
+				break;
+		}
+		return daysbefore;
+    }
+
     public int calculateQuote(int days){
     	int quoteNumber = days % 6;
-    	System.out.println("Quotenumber is: " + quoteNumber);
     	return quoteNumber;
     }
 
 	public static void main(String[] args) {
 		java_Quote quote = new java_Quote();
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a number:");
-		int number = input.nextInt();
 		int daysYear = quote.getDate();
 		int quoteNumber = quote.calculateQuote(daysYear);
 		quote.chooseQuote(quoteNumber);
